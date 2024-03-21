5814ef49bcd86d80efddad209af45554775db7b4
blamersj30 <152201467+blamersj30@users.noreply.github.com>
2024-03-21 16:06:54 -0500

    Fixed da stuff
    Finally. My main man Claude highkey clutched up big time with helping me figure out why find and find_all couldn't find strings. He taught me all I know 💯

--- Scraper.py
+++ Scraper.py
@@ -4,9 +4,24 @@ from bs4 import BeautifulSoup
 url = "https://www.scrapethissite.com/pages/simple/"
 site = requests.get(url)
 
+
 sitehtml = BeautifulSoup(site.text.lower(), features="html.parser")
 idk = sitehtml.prettify()
 
-#test = sitehtml.find_all("span", {"class": "country-population"})
-test = sitehtml.find_all('h3 class="country-name"')
-print(test)
+test = sitehtml.find_all("div", class_="col-md-4 country")
+
+while True:
+    country = input("What country would you like to find the population of?: ").lower()
+    if country == "done": #gives up
+        break
+    if country == "usa" or country == "united states of america" or country == "us": #USA NUMBER 1 RAHHHHHH
+        country = "united states"
+    
+    #Just like loops thru the different country class div's frfr no cap
+    for div in test:
+        countryclass = div.find("h3", class_="country-name")
+        
+        #Kinda just does some silly stuff
+        if country == countryclass.text.strip():
+            countrypop = div.find("span", class_="country-population")
+            print("The population of " + country + " is: " + countrypop.text.strip())
